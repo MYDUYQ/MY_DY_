19b71cf4770003ca92c2e32fae3823b84097c804
MYDUYQ <my070310@163.com>
2025-10-23 16:10:31 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
+//202510125113
+//my070310@163.com
+//李雪峰
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
+    float c=0;
+    printf("请输入摄氏度（度）");
+    scanf("%f",&c);
+    float f=c*9/5+32;
+    printf("%.1f华摄氏度",f);
     return 0;
 }
