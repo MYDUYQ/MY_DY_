934cff3b2c2ef0edf25eec36a6043cf3aef59d87
MYDUYQ <my070310@163.com>
2025-10-23 17:20:57 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,24 +2,28 @@
 //my070310@163.com
 //李雪峰
 #include <stdio.h>
+
 int main()
 {
-    int c=0;
-    printf("请输入数学成绩（0-100）\n");
-    scanf("%d",&c);
-    if(c>=90&&c<=100){
-        printf("等级为A\n");}
-        else if(c>=80&&c<=89){
-            printf("等级为B");
-        }
-        else if(c>=70&&c<=79){
-            printf("等级为C");
-        }
-        else if(c>=60&&c<=69){
-            printf("等级为D");
-        }
-        else if(c<=60){
-            printf("等级为E");
-        }
-    return 0;
+    int a;
+    int b;
+    char ch;
+    printf("请输入两个个整数和一个运算符");
+    scanf("%d %d %c",&a,&b,&ch);
+    switch (ch)
+    {
+    case '+':
+       printf("结果是%d",a+b);
+        break;
+        case '-':
+       printf("结果是%d",a-b);
+        break;
+        case '*':
+       printf("结果是%d",a*b);
+        break;
+        case '/':
+       printf("结果是%d",a/b);
+        break;
+    }
+return 0;
 }
