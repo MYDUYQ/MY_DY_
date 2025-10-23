14dbd2e14085f89ed9d97695f45825065043d3c1
MYDUYQ <my070310@163.com>
2025-10-23 17:41:54 +0800

    Create 4_main.c

---
+++ 4_main.c
@@ -0,0 +1,13 @@
+//202510125113
+//my070310@163.com
+//李雪峰
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+if(a+b>c&&a-b<c)
+{printf("能组成三角形");}
+else{printf("不能组成三角形");}
+return 0;
+}
