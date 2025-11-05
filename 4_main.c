5de6edfc61ce125bf55bce3521cc6bc6f1e9c54b
MYDUYQ <my070310@163.com>
2025-11-05 19:12:36 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -2,18 +2,22 @@
 //my070310@163.com
 //李雪峰
 #include <stdio.h>
-
+int one(int a,int b)
+{
+    int c=1;
+    for(int i=0;i<b;i++)
+    {//c=c*a
+        c*=a;
+    }
+    return c;
+}
 int main()
 {
-   int m=100;
-   while(m<=999)
-   {int a=m%10;
-   int b=m/10%10;
-   int c=m/100;
-    if (m==a*a*a+b*b*b+c*c*c)
-   {
-    printf("%d ",m);
-   }m++;}
-   
+    int d=0;
+    for(int i=1;i<6;i++)
+    {
+        d+=one(i,2);
+    }
+    printf("%d\n",d);
     return 0;
 }
