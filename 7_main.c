30291a4c04ae0b51e6bbe8ca03b67ea32a8e80b2
MYDUYQ <my070310@163.com>
2025-11-05 21:24:38 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -1,27 +1,30 @@
 //202510125113
 //my070310@163.com
 //李雪峰
-int main()
+#include <stdio.h>
+int one ()
 {
     int a[5];
-    int i;
-    for (i = 0; i <4;i++)
-    {
-       scanf("%d",&a[i]);
-    }
-       for (i=4;i>0;i--)
-       {
-       a[i]=a[i-1];
-       }
-       a[0]=0;
-       for (i= 0; i < 5; i++)
-       {
-         if (i == 4) {
+    for (int i = 0; i <5; i++)
+{
+scanf("%d",&a[i]);
+}
+for(int j;j<2;j++)
+{
+    int b;
+    b=a[j];
+    a[j]=a[4-j];
+    a[4-j]=b;
+}
+ for (int i = 0; i < 5; i++) {
+        if (i == 4) {
             printf("%d", a[i]);  
         } else {
             printf("%d ", a[i]); 
-        };
-       }
-      
+        }}
+}
+int main()
+{
+    one();
     return 0;
 }
