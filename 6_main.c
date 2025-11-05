e9a7da73ad67b86bea70a7f66fb1e0707d812e23
MYDUYQ <my070310@163.com>
2025-11-05 20:02:47 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -2,24 +2,21 @@
 //my070310@163.com
 //李雪峰
 #include <stdio.h>
-
-int main() {
-    int a[5]; 
-    int count = 0; 
-    int num;  
-    while (count < 5) {
-        scanf("%d", &num);
-        if (num % 2 == 0) {
-            a[count] = num;
-            count++;  
-        }
-    }
-    for (int i = 0; i < 5; i++) {
-        if (i == 4) {
-            printf("%d", a[i]);  
-        } else {
-            printf("%d ", a[i]); 
-        }
-    }
+int one ()
+{
+    int a[5];
+    for (int i = 0; i <5; i++)
+{
+scanf("%d",&a[i]);
+}
+int b;
+b=a[1]+a[2]+a[3]+a[0]+a[4];
+int c;
+c=a[1]*a[3]*a[0]*a[4]*a[2];
+printf("%d %d",b,c);
+}
+int main()
+{
+    one();
     return 0;
 }
