f25a710ef61105ad57a37ea5325669ed641c5587
MYDUYQ <my070310@163.com>
2025-11-05 21:32:15 +0800

    Update 7_main.c

--- 7_main.c
+++ 7_main.c
@@ -2,29 +2,29 @@
 //my070310@163.com
 //李雪峰
 #include <stdio.h>
-int one ()
-{
+// 用于实现数组操作的函数
+void one() {
     int a[5];
-    for (int i = 0; i <5; i++)
-{
-scanf("%d",&a[i]);
-}
-for(int j;j<2;j++)
-{
-    int b;
-    b=a[j];
-    a[j]=a[4-j];
-    a[4-j]=b;
-}
- for (int i = 0; i < 5; i++) {
+    // 正确输入数组元素
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+    // 正确反转数组
+    for (int i = 0; i < 2; i++) {
+        int temp = a[i];
+        a[i] = a[4 - i];
+        a[4 - i] = temp;
+    }
+    // 输出数组
+    for (int i = 0; i < 5; i++) {
         if (i == 4) {
-            printf("%d", a[i]);  
+            printf("%d", a[i]);
         } else {
-            printf("%d ", a[i]); 
-        }}
+            printf("%d ", a[i]);
+        }
+    }
 }
-int main()
-{
+int main() {
     one();
     return 0;
 }
