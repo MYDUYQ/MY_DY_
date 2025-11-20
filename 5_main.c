db30b4c674846972d8c5a0db3e704e7825d8d6c8
MYDUYQ <my070310@163.com>
2025-11-20 16:50:31 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -1,17 +1,25 @@
 //202510125113
 //my070310@163.com
 //李雪峰
-#include <stdio.h>
-int one(int a1,int an,int step)
-{
-int a=step*(a1+an)/2;
-return a;
+ #include <stdio.h>
+void swap(int len, int*ptr)
+ {
+  for (int i = len-1; i >0; i--)
+ {
+   ptr[i]=ptr[i-1];
+ }
+ ptr[0]=0;
 }
-
 int main()
+{int a[5];
+   for (int i = 0; i <5; i++)
 {
-    int a;
-    a=one (1,100,100);
-    printf("%d",a);
+scanf("%d",&a[i]);
+}
+  swap(5,a); 
+  for (int i = 0; i <5; i++)
+ {
+   printf("%d ",a[i]);
+ }
     return 0;
 }
