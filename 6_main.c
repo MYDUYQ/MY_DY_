540fc255ffbe7d19731bb09f07a06fd0ef2fc587
MYDUYQ <my070310@163.com>
2025-11-20 16:51:30 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -1,22 +1,21 @@
 //202510125113
 //my070310@163.com
 //李雪峰
-#include <stdio.h>
-int one ()
-{
-    int a[5];
-    for (int i = 0; i <5; i++)
-{
-scanf("%d",&a[i]);
-}
-int b;
-b=a[1]+a[2]+a[3]+a[0]+a[4];
-int c;
-c=a[1]*a[3]*a[0]*a[4]*a[2];
-printf("%d %d",b,c);
-}
-int main()
-{
-    one();
+#include <stdlib.h> // 包含malloc/free函数
+
+int main() {
+    // 动态申请5个int的内存空间
+    int *arr = (int *)malloc(5 * sizeof(int));
+    // 输入数据
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // 遍历输出
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    // 释放内存
+    free(arr);
+    arr = NULL; // 避免野指针
     return 0;
 }
